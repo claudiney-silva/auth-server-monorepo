4139b629ec44dd8697a6eb47c15075670bcf0a12
Claudiney Calixto da Silva <clau.li.erd@gmail.com>
2023-05-01 10:15:21 -0300

    chore: Criptografia algoritmos

---
+++ apps/crytpo-server/src/main/java/com/github/claudineysilva/crytposerver/CrytpoServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.claudineysilva.crytposerver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrytpoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrytpoServerApplication.class, args);
+	}
+
+}
---
+++ apps/crytpo-server/src/test/java/com/github/claudineysilva/crytposerver/CrytpoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.claudineysilva.crytposerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrytpoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
